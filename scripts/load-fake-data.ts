458bfd804060582dcf84f958cbbefcf58f54b2cf
Tajry <jirayutchanunto1234@gmail.com>
2024-01-15 10:05:49 +0700

    add .env database

--- scripts/load-fake-data.ts
+++ scripts/load-fake-data.ts
@@ -1,6 +1,28 @@
-function loadFakeData(numUser: number = 10) {
+import { Client } from "pg";
+import { loadEnvConfig } from "@next/env";
+
+
+const projectdir = process.cwd();
+loadEnvConfig(projectdir);
+
+async function loadFakeData(numUser: number = 10) {
     console.log("eucte load fake daa .generting "+ numUser + "users" );
 
+
+    const client = new Client({
+        user: process.env.POSTGRES_USER,
+        host:process.env.POSTGRES_HOST,
+        database:process.env.POSTGRES_NAME,
+        password:process.env.POSTGRES_PASSWORD,
+        port:5432
+    });
+    await client.connect();
+
+    const res = await client.query("select 1")
+
+    console.log(res)
+
+    await client.end();
 }
 
 
