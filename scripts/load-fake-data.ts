469b5451e8757cfff4aaddeca4e2b67c87def82e
Tajry <jirayutchanunto1234@gmail.com>
2024-01-15 11:26:48 +0700

    gitcomplate load fake data

--- scripts/load-fake-data.ts
+++ scripts/load-fake-data.ts
@@ -1,5 +1,6 @@
 import { Client } from "pg";
 import { loadEnvConfig } from "@next/env";
+import {faker } from '@faker-js/faker';
 
 
 const projectdir = process.cwd();
@@ -16,14 +17,64 @@ async function loadFakeData(numUser: number = 10) {
         password:process.env.POSTGRES_PASSWORD,
         port:5432
     });
-    await client.connect();
 
-    const res = await client.query("select 1")
+    await client.connect()
 
-    console.log(res)
+    try {
+        await client.query('begin');
 
-    await client.end();
+        for (let i = 0;i< numUser ;i++) {
+            await client.query("insert into public.users(username ,password ,avatar) values($1 ,$2 ,$3 )",
+            [faker.internet.userName(),"password",faker.image.avatar()])
+        }
+
+
+        const res = await client.query(
+            "select id from users order by created_at desc limit $1",
+            [numUser]
+        );
+
+        console.log(res.rows);
+
+
+        for(const row of res.rows) {
+            for (let i = 0; i< Math.ceil(Math.random() * 50); i++) {
+                await client.query(
+                    "insert into public.posts (user_id ,content) values($1 ,$2)",
+                    [row.id ,faker.lorem.sentence()]
+                )
+            }
+        }
+
+
+        for(const row1 of res.rows) {
+            for(const row2 of res.rows) {
+                if (row1.id != row2.id) {
+                    if (Math.random() > 0.5) {
+                        await client.query(
+                            "insert into follows (user_id ,follower_id) values($1 ,$2)",
+                            [row1.id ,row2.id]
+                        )
+                    }
+                }
+            }
+        }
+
+        await client.query('commit');
+    }catch (error) {
+        await client.query("rollback");
+        throw error
+
+    }finally {
+        await client.end();
+    }
+
+
+
+
+
 }
 
+let numUser = parseInt(process.argv[2]) || 10;
 
-loadFakeData();
+loadFakeData(numUser);
