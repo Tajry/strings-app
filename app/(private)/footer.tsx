1cb9456008679bf9af483d112a21e8fbf93dd4ce
Tajry <jirayutchanunto1234@gmail.com>
2024-01-18 09:49:06 +0700

    navbar and footer

--- app/(private)/footer.tsx
+++ app/(private)/footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer>
-        Footer
+        &copy;Strings {new Date().getFullYear()}
     </footer>
   )
 }
--- app/(private)/navbar.tsx
+++ app/(private)/navbar.tsx
@@ -1,7 +1,23 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Navbar() {
   return (
-    <nav>Nav Bars</nav>
+    <nav className='flex max-w-md w-full p-5 bg-slate-800 rounded-lg my-2'>
+      <ul className='flex flex-row justify-around w-full'>
+        <li>
+          <Link href={'/feed'}>feed</Link>
+        </li>
+        <li>
+          <Link href={'/profile'}>profile</Link>
+        </li>
+        <li>
+          <Link href={'/following'}>following</Link>
+        </li>
+        <li>
+          <Link href={'/follower'}>follower</Link>
+        </li>
+      </ul>
+    </nav>
   )
 }
