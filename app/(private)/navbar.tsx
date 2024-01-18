be62f8ad66a5d58643cc3eb59ac3f583919f9cd1
Tajry <jirayutchanunto1234@gmail.com>
2024-01-18 10:00:30 +0700

    Active Link

--- app/(private)/navbar.tsx
+++ app/(private)/navbar.tsx
@@ -1,21 +1,35 @@
 import Link from 'next/link'
 import React from 'react'
+import { usePathname,  } from 'next/navigation'
+
 
 export default function Navbar() {
+
+  const pathname = usePathname();
+
+
   return (
     <nav className='flex max-w-md w-full p-5 bg-slate-800 rounded-lg my-2'>
       <ul className='flex flex-row justify-around w-full'>
         <li>
-          <Link href={'/feed'}>feed</Link>
+          <Link 
+          className={pathname.startsWith("/feed") ? "text-green-400" : ""}
+          href={'/feed'}>feed</Link>
         </li>
         <li>
-          <Link href={'/profile'}>profile</Link>
+          <Link 
+            className={pathname.startsWith("/profile") ? "text-green-400" : ""}
+          href={'/profile'}>profile</Link>
         </li>
         <li>
-          <Link href={'/following'}>following</Link>
+          <Link
+           className={pathname.startsWith("/following") ? "text-green-400" : ""}
+          href={'/following'}>following</Link>
         </li>
         <li>
-          <Link href={'/follower'}>follower</Link>
+          <Link
+           className={pathname.startsWith("/follower") ? "text-green-400" : ""}
+          href={'/follower'}>follower</Link>
         </li>
       </ul>
     </nav>
