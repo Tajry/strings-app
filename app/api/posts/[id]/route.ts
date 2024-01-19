a3e41c9221cd455104c5c07de50b6a9f11f17f9f
Tajry <jirayutchanunto1234@gmail.com>
2024-01-19 09:37:14 +0700

    udate posts endpoing

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -14,3 +14,20 @@ export async function GET(requst:Request , {params}:{params:{id:number}}) {
 
     return NextResponse.json({data:res.rows[0]})
 }
+
+
+export async function PATCH(request:Request , {params}:{params:{id:number}}) {
+    const body = await request.json();
+    const jwtPlayload = await getJWTPayload()
+    const res = await sql("select * from posts where user_id = $1 and id = $2 ",
+    [params.id , jwtPlayload.sub]
+    )
+    console.log(body)
+    if (res.rowCount == 0) {
+        return NextResponse.json({error:'not found'} ,{status:404}) 
+    }
+    await sql("update posts set content =  $1 where user_id = $2 and id = $3 " ,
+    [body.content , jwtPlayload.sub , params.id]
+    )
+    return NextResponse.json({msg:"update success "},{status:200})
+}
