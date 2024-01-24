1bf4b5ea8d7f62a7881e02e081693395b4b38c4a
Tajry <jirayutchanunto1234@gmail.com>
2024-01-24 10:45:57 +0700

    flag misinformation endpoint

--- app/util/auth.ts
+++ app/util/auth.ts
@@ -1,5 +1,7 @@
+import { sql } from '@/db';
 import { jwtVerify } from 'jose';
 import {cookies} from 'next/headers';
+import { NextResponse } from 'next/server';
 export async function getJWTPayload() {
     const cookieStore =  cookies()
     const token = await cookieStore.get('jwt-token')
@@ -7,4 +9,14 @@ export async function getJWTPayload() {
     const secret = new TextEncoder().encode(process.env.JWT_SECRET);
     const {payload , protectedHeader} = await jwtVerify(tokens! ,secret)
     return payload;
+}
+
+export async function authorizeAdmin(func:Function) {
+    const jwtPayload = await getJWTPayload();
+    const res = await sql("select is_admin from users where id = $1" ,[jwtPayload.sub])
+    const data  = res.rows[0];
+    if (!data.is_admin) {
+        return NextResponse.json({error:'unauthorzed'}, {status:4043})
+    }
+    return func()
 }
